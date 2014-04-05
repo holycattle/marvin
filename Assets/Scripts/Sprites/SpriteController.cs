5d8386ecd2ccff32c03e0d2337fad47f61a0db95
Joniel Ibasco <jlibasco@gmail.com>
2014-04-05 21:33:11 +0800

    Got sprites to work.

---
+++ Assets/Scripts/Sprites/SpriteController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteController : MonoBehaviour {
+	public Sprite[] sprites;
+	private SpriteRenderer spriteRenderer;
+	private int spriteIndex;
+	public int framesPerSecond;
+
+	public bool isAnimated;
+
+	void Start () {
+		spriteRenderer = renderer as SpriteRenderer;
+		spriteIndex = 0;
+
+		//TODO: Have a central config for stuff like frame rate
+		// framesPerSecond = CONFIG.framesPerSecond
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(isAnimated) {
+			changeFrame((int)Time.timeSinceLevelLoad * framesPerSecond);
+		}
+	}
+
+	public void changeFrame(int frameRate) {
+		spriteIndex = spriteIndex % sprites.Length;
+		spriteRenderer.sprite = sprites[spriteIndex];
+	}
+}
