ea7e15eb8207c7e58243258267127eef7b8ee69e
unknown <Josh@Josh-PC.(none)>
2014-04-05 08:21:08 +0800

    folder structure added

---
+++ Assets/Scripts/Triggers/Trigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Trigger : MonoBehaviour {
+
+	public TriggerAction[] triggers;
+
+	void Start() {
+		Debug.Log("Trigger Started!");
+	}
+
+	void OnTriggerEnter(Collider c) {
+		Debug.Log("Collisioned!");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Collisioned@2D!");
+	}
+
+	public void PullTrigger() {
+	}
+}
