2b13d4ad9d567d5f9651b688f1c27b48deb2d334
unknown <Josh@Josh-PC.(none)>
2014-04-22 19:34:22 +0800

    More editor tools

---
+++ Assets/Editor/GraphEditorWindow.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class GraphEditorWindow : EditorWindow {
+	TriggerAction[] actions;
+	Rect[] actionRects;
+
+	[MenuItem ("Window/Graph Editor Window")]
+	static void Init() {
+		EditorWindow.GetWindow(typeof(GraphEditorWindow));
+	}
+	
+	void OnGUI() {
+		// Draw the Connections
+//		Handles.BeginGUI();
+//		Handles.DrawBezier(windowRect.center, windowRect2.center, //
+//		                   new Vector2(windowRect.xMax + 50f, windowRect.center.y), //
+//		                   new Vector2(windowRect2.xMin - 50f, windowRect2.center.y), //
+//		                   Color.red, null, 5f);
+//		Handles.EndGUI();
+		
+		BeginWindows();
+		if (actionRects != null) {
+			for (int i = 0; i < actionRects.Length; i++) {
+				actionRects[i] = GUI.Window(i, actionRects[i], WindowFunction, actions[i].gameObject.name);
+			}
+		}
+
+		if (GUI.Button(new Rect(0, 0, 100, 30), "Sync")) {
+			SyncObjects();
+		}
+
+		EndWindows();
+	}
+
+	void WindowFunction(int windowID) {
+		if (GUI.Button(new Rect(0, 20, 100, 20), "[Select]")) {
+			Debug.Log("Selected!");
+			Selection.objects = new Object[]{ actions[windowID].gameObject };
+		}
+
+		// Allow for window dragging
+		GUI.DragWindow();
+	}
+
+	public void SyncObjects() {
+		// Get all the objects in the scene of type Action
+		actions = GameObject.FindObjectsOfType<TriggerAction>();
+		actionRects = new Rect[actions.Length];
+		for (int i = 0; i < actionRects.Length; i++) {
+			actionRects[i] = new Rect(100, 100, 150, 80);
+		}
+	}
+}
---
+++ Assets/Scripts/TriggerSystem/TriggerActions/TextAction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextAction : TriggerAction {
+
+	public GUISkin textSkin;
+	public bool isAttachedToSound = false;
+	public float duration = 4f;
+	public string text;
+	public Rect textArea;
+
+	//
+	private float timeStarted = -1f;
+
+	void Start() {
+		textArea = new Rect(textArea.x * Screen.width, textArea.y * Screen.height, textArea.width * Screen.width, textArea.height * Screen.height);
+		if (isAttachedToSound) {
+			AudioClip c = GetComponent<SoundAction>().oneShotClip;
+			duration = c.length;
+		}
+	}
+
+	void OnGUI() {
+		if (timeStarted >= 0) {
+			GUISkin gsk = GUI.skin;
+			GUI.skin = textSkin;
+			GUI.Label(textArea, text);
+			GUI.skin = gsk;
+
+			if (timeStarted + duration <= Time.time) {
+				timeStarted = -1;
+			}
+		}
+	}
+
+	public override void ExecuteAction(TriggerAction trigger, GameObject invoker) {
+		timeStarted = Time.time;
+	}
+
+}
